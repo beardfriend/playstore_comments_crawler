6c5c7feb183f0430c360c5d56e1d63fcca6c9511
beardfriend <beardfriend21@gmail.com>
2023-04-19 10:49:46 +0900

    feat: prompt, crawl comments basic

--- crawler.py
+++ crawler.py
@@ -1,6 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 from urllib.parse import quote
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
+import re
 
 
 class Crawler:
@@ -19,6 +25,7 @@ class Crawler:
         }
 
         self.soup = None
+        self.driver = None
 
     def getInfoByname(self, name):
         pageInfo = self.playStore["listPage"]
@@ -32,7 +39,7 @@ class Crawler:
         if container == None:
             appId, title, madeBy = self._inCorrectQuery()
         else:
-            appId, title, madeBy = self._correctQuery()
+            appId, title, madeBy = self._correctQuery(container)
 
         self._resetSoup()
 
@@ -67,12 +74,92 @@ class Crawler:
     def _resetSoup(self):
         self.soup = None
 
-    def crawlComments(self, startDate, endDate, minStarredCount, maxStarredCount, howmany):
-        
-        
+    def crawlComments(
+        self,
+        appId,
+        startDate="1",
+        endDate="2",
+        minStarredCount="1",
+        maxStarredCount="5",
+        howmany=200,
+    ):
+        pageInfo = self.playStore["detailPage"]
+        url = pageInfo["defaultUrl"] + appId + pageInfo["basicQuery"]
+
+        self._setSelenium()
+        self.driver.get(url)
+
+        # click
+        button = self.driver.find_element(
+            By.XPATH,
+            '//*[@id="yDmH0d"]/c-wiz[2]/div/div/div[1]/div[2]/div/div[1]/c-wiz[4]/section/header/div/div[2]/button/i',
+        )
+
+        ActionChains(self.driver).move_to_element(button).pause(1).click().pause(
+            1
+        ).perform()
+
+        time.sleep(2)
+        commentEles = self.driver.find_element(
+            By.XPATH,
+            '//*[@id="yDmH0d"]/div[4]/div[2]/div/div/div/div/div[2]',
+        )
+
+        # 첫 번째 방법 요소 길이를 구하고
+        # 요소 길이를 구하고 길이가 다 되면 스크롤해서 길이부터 계속 크롤링
+        collectedCount = 0
+
+        parent = commentEles.find_element(By.XPATH, "./div/div[1]")
+
+        while collectedCount < howmany:
+            length = len(parent.find_elements(By.XPATH, "./*"))
+            for i in range(collectedCount, length):
+                element = parent.find_element(By.XPATH, f"./div[{i+1}]")
+
+                # 유저이름
+                userName = element.find_element(
+                    By.XPATH, "./header/div[1]/div[1]/div"
+                ).text
+
+                # 별점
+                starred = element.find_element(
+                    By.XPATH, "./header/div[2]/div"
+                ).get_attribute("aria-label")
+
+                # 리뷰시점
+                reviewedAt = element.find_element(By.XPATH, "./header/div[2]/span").text
+
+                # 리뷰 내용
+                content = element.find_element(By.XPATH, "./div[1]").text
+
+                # 유용하다고 평가한 사람의 수
+                usefulCount = 0
+                try:
+                    usefulCount = re.sub(
+                        r"[^0-9]",
+                        "",
+                        element.find_element(By.XPATH, "./div[2]/div").text,
+                    )
+                except:
+                    continue
+
+                print(usefulCount)
+
+            collectedCount = length
+
+            self._scrollDown(commentEles)
+
+    def _scrollDown(self, parent):
+        scroll_origin = ScrollOrigin.from_element(parent)
+        ActionChains(self.driver).scroll_from_origin(scroll_origin, 0, 10000).perform()
+
+    def _setSelenium(self):
+        options = webdriver.ChromeOptions()
+        # options.add_argument("headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        self.driver = webdriver.Chrome("./chromedriver", options=options)
 
     # def _savePoint():
 
     # def checkprogressed():
-
-
--- main.py
+++ main.py
@@ -3,7 +3,20 @@ from crawler import Crawler
 
 def main():
     playStoreCrawler = Crawler()
-    result = playStoreCrawler.getTopResultByname("화해해")
+    appName = input("크롤링할 앱 이름을 입력해주세요 : ")
+    result = playStoreCrawler.getInfoByname(appName)
+
+    print("------------------------")
+    print(result["name"])
+    print("------------------------")
+
+    isContinue = input("크롤링할 앱이 맞나요? (y | n): ")
+
+    if isContinue == "y":
+        playStoreCrawler.crawlComments(result["appId"])
+    else:
+        print("재실행 해주세요 !! :) ")
+        quit()
 
 
 main()
